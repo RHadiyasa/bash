95fb727a271ad70fa5779557278ae63a04bc2ef6
Rafi Hadiyasa <rafihadiyasa32@gmail.com>
2024-06-08 17:51:43 +0700

    Add dark theme

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
-import { Inter } from "next/font/google";
+import { Inter as Fontsans } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "next-themes";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = Fontsans({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +13,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.className
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ src/app/login/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const loginPage = () => {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <div>Login Page</div>      
+      <Button>Ini Button</Button>
+    </div>
+  );
+};
+
+export default loginPage;
