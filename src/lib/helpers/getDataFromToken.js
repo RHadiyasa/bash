2bc8cfa496c577eebe11144e7cfdfae8a5ae5123
Suryo Hastomo <ryohastomo@gmailcom>
2025-03-08 19:41:57 +0700

    fix: cors origin

--- src/lib/helpers/getDataFromToken.js
+++ src/lib/helpers/getDataFromToken.js
@@ -2,10 +2,10 @@ import jwt from "jsonwebtoken";
 
 export const getDataFromToken = (request) => {
   try {
-    const tokenCookies = request.cookies.get("token")?.value || "";
+    const tokenCookies = request.cookies.get("token")?.value;
     const authHeader = request.headers.get("authorization");
 
-    const token = tokenCookies;
+    const token = tokenCookies ?? authHeader?.split(" ")[1];
 
     const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
     return decodedToken.id;
